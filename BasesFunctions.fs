e3404c7475b924ed076becf5454b59622f04cc56
Daniel Markham <danielbmarkham@hotmail.com>
2023-09-02 11:09:06 -0400

    got happy path working front to back need to add different and perhaps weird bases

--- BasesFunctions.fs
+++ BasesFunctions.fs
@@ -1,7 +1,8 @@
 module BasesFunctions
 open System
+open System.Text
 open System.Numerics
-
+open FSharp.Collections
 
 let foobar() = printfn "\nMODULE LOADED\n"
 
@@ -19,15 +20,26 @@ let parseCLIOptionsForNumberBase iArgIndex = // DISCRIMINATED UNION ARG HELPER N
 let isScientificNotation numberString = System.Text.RegularExpressions.Regex.Match(numberString, "[e|E][\+\-]").Length=2
 let splitByScientificNotation numberString = System.Text.RegularExpressions.Regex.Split(numberString, "[e|E][\+\-]")
 
-
+let convertNumberToBaseNString srcNumber (srcBase:NumberBase) = 
+  "8"
+let convertBaseNStringToNumber srcNumber (srcBase:NumberBase) = 
+  "2"
 
 let convertFromBigIntegerToBaseX (bignumber:bigint) destBase =
   printfn "IM HERE"
   let numDigits= bigint.Log destBase
   let startingExponent = (int)numDigits+1
+  let resultBuffer=new System.Text.StringBuilder(4096)
   printfn "numDigits %A" startingExponent
-  [for x in 1..startingExponent do yield startingExponent-x] |> Seq.iteri(
-    fun i x->printfn "zonk %A %A" i x
-    
-    )
-  ""
+  [for x in 1..startingExponent do yield startingExponent-x] 
+    |> Seq.fold(
+      fun (acc:BigInteger*StringBuilder) x->
+        let dec=(fst acc)/(destBase**x)
+        printfn "zonk %A %A" x dec
+        let remainder = (fst acc)%(destBase**x)
+        resultBuffer.Append (string dec ) |> ignore
+        (remainder,resultBuffer)
+      ) (bignumber, resultBuffer)
+    |>ignore
+  printfn "THE ANSWER IS %A" resultBuffer
+  resultBuffer
